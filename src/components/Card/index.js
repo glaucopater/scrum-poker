0c582380bb1526e1a32f92c9599c7c8bfd3eca7f
Glauco Pater <>
2020-02-25 23:50:37 +0100

    Change to alt prop

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -12,12 +12,12 @@ const Card = ({ value }) => {
     <SCCard className={(selected ? "picked" : "")} onClick={handleOnClick}>
       <div className="front">
         <span>{value}</span>
-        <img src={logo} alt={"front"} />
+        <img src={logo} alt="" />
         <span>{value}</span>
       </div>
       <div className="back">
         <span>{value}</span>
-        <img src={logo} alt={"back"} />
+        <img src={logo} alt="" />
         <span>{value}</span>
       </div>
     </SCCard>
